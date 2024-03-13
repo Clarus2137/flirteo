252a578c51f09e5a0c83b4370b1accc425be3bc5
Aleks <a.kudriakov@web-systems.pl>
2024-03-13 15:46:28 +0100

    Fixing registration started - 13.03.2024

--- src/components/EnterEmail.vue
+++ src/components/EnterEmail.vue
@@ -1,59 +1,92 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useUserStore } from 'src/stores/user';
+import { useRouter } from 'vue-router';
+// import { useUserStore } from 'src/stores/user';
 
 
 const title = {
-   title: 'Create your account',
-   subtitle: 'Please enter your e-mail & password to continue'
+    title: 'Create your account',
+    subtitle: 'Please enter your e-mail & password to continue'
 }
 
 const validEmail = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
-
 const enteredEmail = ref('');
+const enteredPassword = ref('');
 
-const isVisible = ref(false);
+const isVisibleEmail = ref(false);
+const isVisiblePassword = ref(false);
 
-const checkEmail = (e: Event) => {
-   if (!validEmail.test(enteredEmail.value)) {
-      // Showing alarm if E-mail is invalid
-      !isVisible.value ? isVisible.value = !isVisible.value : '';
-      e.preventDefault();
-   } else {
-      // Hiding alarm if E-mail is valid
-      isVisible.value ? isVisible.value = !isVisible.value : '';
-   }
+const goToGeneralInfo = useRouter();
+
+const validateEmailAndPassword = (): boolean => {
+    let isValid = true;
+
+    // Reset visibility states
+    isVisibleEmail.value = false;
+    isVisiblePassword.value = false;
+
+    if (!validEmail.test(enteredEmail.value)) {
+        isVisibleEmail.value = true;
+        isValid = false;
+    }
+
+    const hasValidLength = enteredPassword.value.length >= 8;
+    const hasUpperCase = /[A-Z]/.test(enteredPassword.value);
+    const hasNumber = /[0-9]/.test(enteredPassword.value);
+    const hasSpecialSymbol = /[-_!?&$#@]/.test(enteredPassword.value);
+
+    if (!hasValidLength || !hasUpperCase || !hasNumber || !hasSpecialSymbol) {
+        isVisiblePassword.value = true;
+        isValid = false;
+    }
+
+    return isValid;
 }
 
-const userStore = useUserStore();
+const handleSubmit = (e: Event) => {
 
-const createUser = () => {
-   const newUser = {
-      email: enteredEmail.value
-   }
-
-   userStore.createUser(newUser);
-
-   enteredEmail.value = '';
+    if (!validateEmailAndPassword()) {
+        e.preventDefault(); // Prevent form from submitting by default
+        console.log('Form is invalid');
+    } else {
+        console.log('form sent');
+        goToGeneralInfo.push('/info/general');
+    }
 }
+
+// const userStore = useUserStore();
+
+// const createUser = () => {
+//    const newUser = {
+//       email: enteredEmail.value
+//    }
+
+//    userStore.createUser(newUser);
+
+//    enteredEmail.value = '';
+// }
 </script>
 
 
 
 <template>
-   <BackBtn />
-   <div class="email">
-      <div class="email__title">
-         <TitleRow :title="title" />
-      </div>
-      <form class="email__form mt-5" id="reg-account" @submit="checkEmail($event)">
-         <CustomInput type="text" placeholder="example@domain.com" v-model="enteredEmail" required />
-         <p class="text-center text-xs text-alarm font-bold duration-300"
-            :class="{ 'opacity-100': isVisible, 'opacity-0': !isVisible }">
-            Invalid E-mail
-         </p>
-         <CustomInput type="password" placeholder="Some password" />
-         <CustomBtn type="submit" class="mt-5" @click="createUser(); $router.push('/info/general')">Continue</CustomBtn>
-      </form>
-   </div>
+    <BackBtn />
+    <div class="email">
+        <div class="email__title">
+            <TitleRow :title="title" />
+        </div>
+        <form class="email__form mt-5" id="reg-account" @submit="handleSubmit">
+            <CustomInput type="text" placeholder="example@domain.com" v-model="enteredEmail" required />
+            <p class="text-center text-xs text-alarm font-bold duration-300"
+                :class="{ 'opacity-100': isVisibleEmail, 'opacity-0': !isVisibleEmail }">
+                Invalid E-mail
+            </p>
+            <CustomInput type="password" placeholder="Some password" v-model="enteredPassword" required />
+            <p class="text-center text-xs text-alarm font-bold duration-300"
+                :class="{ 'opacity-100': isVisiblePassword, 'opacity-0': !isVisiblePassword }">
+                Invalid Password
+            </p>
+            <CustomBtn type="submit" class="mt-5">Continue</CustomBtn>
+        </form>
+    </div>
 </template>
