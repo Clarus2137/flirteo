c6538ba3319a2bbdafbf0b3a84e7aba53038d49c
Aleksandr Kudriakov <clarus2137@gmail.com>
2024-05-07 18:43:09 +0200

    New images - 7.05.2024

---
+++ src/i18n/en.ts
@@ -0,0 +1,6 @@
+export default {
+    createAccount: {
+        title: 'Create your account',
+        subtitle: 'Please enter your e-mail & password to continue',
+    }
+};
---
+++ src/i18n/pl.ts
@@ -0,0 +1,6 @@
+export default {
+    createAccount: {
+        title: 'Załóż konto',
+        subtitle: 'Wprowadź email oraz hasło',
+    }
+};
